d0a26e5a7ae7abeca09ed0411cc7a3385a9ea6ce
KIRUTHIK ARUN S <kiruthiksaravana91@gmail.com>
2025-10-12 09:14:02 +0000

    again it is not loginn

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -3,7 +3,7 @@
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { initializeFirebase } from '@/firebase';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
