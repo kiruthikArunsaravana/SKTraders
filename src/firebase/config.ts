6d289c6770fab36662d261c78434dd472d27b8d1
KIRUTHIK ARUN S <kiruthiksaravana91@gmail.com>
2025-10-15 13:25:55 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,8 @@
-// This file is intentionally left blank. 
-// Firebase will be initialized automatically by App Hosting.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "husktrack1-91176301-abb2d",
+  "appId": "1:770032576241:web:6f31262ff9de2ef6a42b50",
+  "apiKey": "AIzaSyAAd6lIcu5YWernNTd43BS_8qd5LNNxHxg",
+  "authDomain": "husktrack1-91176301-abb2d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "770032576241"
+};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,35 +1,35 @@
-
 'use client';
 
+import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
-import { firebaseConfig as devFirebaseConfig } from '@/firebase/config';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (getApps().length) {
-    return getSdks(getApp());
+  if (!getApps().length) {
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
 
-  // In a Firebase App Hosting env, the following env vars should be available.
-  // https://firebase.google.com/docs/app-hosting/automatic-build-and-deploy#node.js
-  const prodFirebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  };
-
-  const firebaseConfig = prodFirebaseConfig.projectId
-    ? prodFirebaseConfig
-    : devFirebaseConfig;
-
-  const firebaseApp = initializeApp(firebaseConfig);
-
-  return getSdks(firebaseApp);
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
@@ -45,5 +45,6 @@ export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
+export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
