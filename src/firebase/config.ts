a83f64c7d76e46c8b69dd572ab291365b0d0fee4
KIRUTHIK ARUN S <kiruthiksaravana91@gmail.com>
2025-10-15 12:55:19 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "husktrack1-91176301-abb2d",
   "appId": "1:770032576241:web:6f31262ff9de2ef6a42b50",
+  "storageBucket": "husktrack1-91176301-abb2d.appspot.com",
   "apiKey": "AIzaSyAAd6lIcu5YWernNTd43BS_8qd5LNNxHxg",
   "authDomain": "husktrack1-91176301-abb2d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "770032576241"
+  "messagingSenderId": "770032576241",
+  "measurementId": "G-5G65G3G3G3"
 };
