26e47901f6a59ae655a6448e405647e5f9d59276
KIRUTHIK ARUN S <kiruthiksaravana91@gmail.com>
2025-10-15 13:18:49 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,3 @@
-
-export const firebaseConfig = {
-  "projectId": "husktrack1-91176301-abb2d",
-  "appId": "1:770032576241:web:6f31262ff9de2ef6a42b50",
-  "storageBucket": "husktrack1-91176301-abb2d.appspot.com",
-  "apiKey": "AIzaSyAAd6lIcu5YWernNTd43BS_8qd5LNNxHxg",
-  "authDomain": "husktrack1-91176301-abb2d.firebaseapp.com",
-  "messagingSenderId": "770032576241",
-  "measurementId": "G-5G65G3G3G3"
-};
+// This file is intentionally left blank. 
+// Firebase will be initialized automatically by App Hosting.
+export const firebaseConfig = {};
