4f10b75f8890cda7264b73c564506e19d07217a9
KIRUTHIK ARUN S <kiruthiksaravana91@gmail.com>
2025-12-14 13:42:55 +0000

    Try fixing this error: `Build Error: Export initializeFirebase doesn't e

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -24,6 +24,5 @@ firestore = getFirestore(firebaseApp);
 
 export { firebaseApp, auth, firestore };
 export * from './provider';
-export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
